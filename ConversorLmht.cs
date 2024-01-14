7a5e36c4247c59d2ac7c0f29273d19159b61ce40
Leonel Sanches da Silva <53848829+leonelsanchesdasilva@users.noreply.github.com>
2024-01-14 10:01:59 -0800

    Apagando "especificacao" para readicionar com letra maiúscula.

--- ConversorLmht.cs
+++ ConversorLmht.cs
@@ -1,10 +1,15 @@
-﻿namespace DesignLiquido.LmhtCSharp
+﻿using System.Xml.Xsl;
+
+namespace DesignLiquido.LmhtCSharp
 {
     public class ConversorLmht
     {
+        private readonly XslCompiledTransform xslt;
+
         public ConversorHtml()
         {
-
+            this.xslt = new XslCompiledTransform(true);
+            this.xslt.Load("./especificacao/lmht10.xslt");
         }
     }
 }
